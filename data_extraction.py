c807567d783d79d97cb37161f4833e3ba5df19af
fusesushan <sushan.kattel@fusemachines.com>
2023-10-27 13:16:40 +0545

    feat: Updated extraction to avoid file redundency

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import yaml
+import os
 
 # Define the path to your YAML file
 yaml_file_path = 'config.yaml'
@@ -16,12 +17,24 @@ except FileNotFoundError:
 if config is not None:
     stocks = ["GOOGL", "AAPL", "AMZN", "META"]
     start_date = "2023-01-01"
+    last_date = "2023-07-01"
     token = config.get("token")
+
+    # Check if the Parquet file already exists
+    parquet_file_path = 'extracted_data/extracted_data.parquet'
+    if os.path.exists(parquet_file_path):
+        # Read the existing parquet file to get the latest timestamp
+        existing_df = pd.read_parquet(parquet_file_path)
+        latest_timestamp = existing_df['t'].max()
+    else:
+        existing_df = None
+        latest_timestamp = 0  # Default value if the file doesn't exist
 
     # Create an empty list to store DataFrames
     stock_data = []
 
     def fetch_stock_data(symbol, start_date, token):
+        # url = f'https://api.marketdata.app/v1/stocks/candles/D/{symbol}?from={start_date}&to={last_date}&token={token}'
         url = f'https://api.marketdata.app/v1/stocks/candles/D/{symbol}?from={start_date}&token={token}'
         try:
             response = requests.get(url)
@@ -43,7 +56,15 @@ if config is not None:
 
     # Fetch data for each stock and store it in the list
     for symbol in stocks:
-        df = fetch_stock_data(symbol, start_date, token)
+        if latest_timestamp == 0 or existing_df is None or existing_df.empty:
+            df = fetch_stock_data(symbol, start_date, token)
+        else:
+            # Only fetch data if it's newer than the latest timestamp in the existing file
+            df = fetch_stock_data(symbol, start_date, token)
+            if existing_df['t'].max() == df['t'].max():
+                df = None
+                print(f"No new data available for {symbol}. Skipping.")
+
         if df is not None:
             df['Stock'] = symbol
             stock_data.append(df)
@@ -52,9 +73,12 @@ if config is not None:
         # Concatenate the DataFrames into a single DataFrame
         df_final = pd.concat(stock_data, axis=0, ignore_index=True)
 
+        if existing_df is not None and not existing_df.empty:
+            # Append new data to the existing DataFrame
+            df_final = pd.concat([existing_df, df_final], ignore_index=True)
+
         # Save the final DataFrame to a Parquet file
-        df_final.to_parquet('extracted_data/extracted_data.parquet', compression='snappy', engine='auto', index=False)
-        # print(df_final)
+        df_final.to_parquet(parquet_file_path, compression='snappy', engine='auto', index=False)
         print("Data saved successfully.")
     else:
         print("No valid data to save.")
