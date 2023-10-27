48db2be383070d969066f36129920f24a2ba9477
fusesushan <sushan.kattel@fusemachines.com>
2023-10-27 10:41:41 +0545

    Added: Data Extraction File

---
+++ data_extraction.py
@@ -0,0 +1,62 @@
+import requests
+import pandas as pd
+import yaml
+
+# Define the path to your YAML file
+yaml_file_path = 'config.yaml'
+
+# Read the YAML file and parse it into a Python dictionary
+try:
+    with open(yaml_file_path, 'r') as yaml_file:
+        config = yaml.safe_load(yaml_file)
+except FileNotFoundError:
+    print("Error: Config file not found.")
+    config = None
+
+if config is not None:
+    stocks = ["GOOGL", "AAPL", "AMZN", "META"]
+    start_date = "2023-01-01"
+    token = config.get("token")
+
+    # Create an empty list to store DataFrames
+    stock_data = []
+
+    def fetch_stock_data(symbol, start_date, token):
+        url = f'https://api.marketdata.app/v1/stocks/candles/D/{symbol}?from={start_date}&token={token}'
+        try:
+            response = requests.get(url)
+
+            if response.status_code in [200, 203]:
+                data = response.json()
+                df = pd.DataFrame(data)
+                df = df.drop('s', axis=1)
+                return df
+            else:
+                print(f"Failed to retrieve data for {symbol}")
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"Request error for {symbol}: {e}")
+            return None
+        except Exception as e:
+            print(f"An error occurred for {symbol}: {e}")
+            return None
+
+    # Fetch data for each stock and store it in the list
+    for symbol in stocks:
+        df = fetch_stock_data(symbol, start_date, token)
+        if df is not None:
+            df['Stock'] = symbol
+            stock_data.append(df)
+
+    if stock_data:
+        # Concatenate the DataFrames into a single DataFrame
+        df_final = pd.concat(stock_data, axis=0, ignore_index=True)
+
+        # Save the final DataFrame to a Parquet file
+        df_final.to_parquet('extracted_data/extracted_data.parquet', compression='snappy', engine='auto', index=False)
+        # print(df_final)
+        print("Data saved successfully.")
+    else:
+        print("No valid data to save.")
+else:
+    print("Error: Configuration not loaded.")
